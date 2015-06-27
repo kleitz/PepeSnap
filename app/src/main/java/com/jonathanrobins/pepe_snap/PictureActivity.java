1b598bc8f704e82b7a65e524b1fd3b16ec219fa7
Jonathan Robins <jonrobns@gmail.com>
2015-06-27 15:16:20 -0400

    fixed navigation bar being missing

--- app/src/main/java/com/jonathanrobins/pepe_snap/PictureActivity.java
+++ app/src/main/java/com/jonathanrobins/pepe_snap/PictureActivity.java
@@ -60,13 +60,6 @@ public class PictureActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         //hides various bars
         getSupportActionBar().hide();
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_picture);
         //intializes buttons and sets colors
         backButton = (Button) findViewById(R.id.backButton);
--- app/src/main/java/com/jonathanrobins/pepe_snap/SplashScreenActivity.java
+++ app/src/main/java/com/jonathanrobins/pepe_snap/SplashScreenActivity.java
@@ -14,13 +14,6 @@ public class SplashScreenActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         //hides various bars
         getSupportActionBar().hide();
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_splash);
 
         Thread background = new Thread() {
